7c8377f71fb9bf153c38e17e49687467bf87210c
mhartopo <muhtarhartopo@gmail.com>
2017-08-30 21:45:00 +0800

    add building controller

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Building;
 
 class BuildingController extends Controller
 {
     //
+    public function index() {
+    	return Building::all();
+    }
+
+    public function show(Building $building) {
+    	return $building;
+    }
+
+    public function store(Request $request) {
+    	$building = Building::create($request->all());
+    	return response()->json($building, 201);
+    }
+
+    public function update(Request $request, Building $building) {
+    	$building->update($request->all());
+    	return response()->json($building, 200);
+    }
+
+    public function delete(Building $building) {
+    	$building->delete();
+    	return response()->json(null, 204);
+    }
 }
