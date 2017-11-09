7b055d9deb846892c2befc7d998610dd895cdf91
mhartopo <muhtarhartopo@gmail.com>
2017-11-09 22:43:56 +0800

    edit semua

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,19 +32,17 @@
                         Diverentia
                     </a>
                 </div>
-
+                <!--
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
+                    
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <!--<li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>-->
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -67,7 +65,7 @@
                             </li>
                         @endif
                     </ul>
-                </div>
+                </div> -->
             </div>
         </nav>
 
