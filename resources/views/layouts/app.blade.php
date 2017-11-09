71ff79100d85fd862c15556a78c28882af0dc3b4
mhartopo <muhtarhartopo@gmail.com>
2017-11-09 22:45:29 +0800

    edit semua

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,13 @@
             <div class="container">
                 <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
+                    <!-- Collapsed Hamburger 
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
-                    </button>
+                    </button> -->
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
