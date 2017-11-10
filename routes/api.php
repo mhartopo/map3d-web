1af9f2a4a150877a6d9d609a2cc1a3321ddaa466
mhartopo <muhtarhartopo@gmail.com>
2017-11-10 10:20:21 +0800

    fix bugs

--- routes/api.php
+++ routes/api.php
@@ -29,10 +29,10 @@ Route::get('buildings/address/{address}', 'BuildingController@getByAddress');
 Route::get('clusters', 'ClusterController@index');
 Route::post('clusters', 'ClusterController@store');
 Route::get('clusters/{cluster}', 'ClusterController@show');
-Route::get('clusters/{cluster}/{objname}', 'ClusterController@getByObject');
 Route::get('clusters/name/{name}', 'ClusterController@getByName');
 Route::get('clusters/address/{address}', 'ClusterController@getByAddress');
 Route::get('clusters/search/nearest', 'ClusterController@getNearest');
+Route::get('clusters/{cluster}/{objname}', 'ClusterController@getByObject');
 
 //land
 Route::get('lands', 'LandController@index');
